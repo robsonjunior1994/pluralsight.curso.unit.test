3e3943a5d6ee0fbd7134356c4cedfcafcdb003fa
robsonjunior1994 <robsonjunior1994@gmail.com>
2024-11-09 17:12:28 -0300

    Aula 07 - Introducing xUnit.Net

---
+++ TestProject/PlayerCharacter Should.cs
@@ -0,0 +1,31 @@
+﻿using pluralsight.curso.unit.test.Models;
+
+namespace TestProject
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            //Arrange
+            PlayerCharacter sut = new PlayerCharacter();
+
+            //Act
+            sut.Sleep();
+
+            //Assert
+            Assert.InRange(sut.Health, 101, 200);
+        }
+
+        [Fact]
+        public void AnotherTest()
+        {
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public void ADataDrivenTest(int valor)
+        {
+        }
+    }
+}
---
+++ pluralsight.curso.unit.test/Models/PlayerCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pluralsight.curso.unit.test.Models
+{
+    public class PlayerCharacter
+    {
+        public int Health { get; set; }
+
+        public PlayerCharacter()
+        {
+            Health = 100;
+        }
+
+        public void Sleep()
+        {
+            var heathIncrease = CalculateHealthIncrease();
+            Health += heathIncrease;
+        }
+
+        private int CalculateHealthIncrease()
+        {
+            var rnd = new Random();
+            return rnd.Next(1, 101);
+        }
+    }
+}
