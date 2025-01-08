aa74a9a938de5d9306c4b91c9ecad99e1f1377d0
Robson Junior <robsonjunior1994@gmail.com>
2025-01-08 08:10:07 -0300

    05 - Argument Matching in Mocked Methods

--- pluralsight.curso.unit.test/CreditCardApplication/CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
+++ pluralsight.curso.unit.test/CreditCardApplication/CreditCardApplication.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -34,13 +34,26 @@ namespace CreditCardApplication.Tests
         public void DeclineLowIncomeApplications()
         {
             var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
-            mockValidator.Setup(x => x.IsValid("x")).Returns(true);
+            //mockValidator.Setup(x => x.IsValid("x")).Returns(true);
+            //mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
+            //mockValidator.Setup(x => x.IsValid(It.Is<string>(x => x.StartsWith("x")))).Returns(true);
+
+            //Inclusive vai incluir as letras A e Z
+            //mockValidator.Setup(x => x.IsValid(It.IsInRange<string>("a", "z", Moq.Range.Inclusive))).Returns(true);
+
+            //Inclusive vai excluir as letras A e Z e começar da B e Y
+            //mockValidator.Setup(x => x.IsValid(It.IsInRange<string>("a", "z", Moq.Range.Exclusive))).Returns(true);
+
+            //mockValidator.Setup(x => x.IsValid(It.IsIn<string>("a", "z", "y"))).Returns(true);
+
+            mockValidator.Setup(x => x.IsValid(It.IsRegex("[a-z]"))).Returns(true);
+
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
             var application = new CreditCardApplication
             {
                 GrossAnnualIncome = 19_999,
                 Age = 42,
-                FrequentFlyerNumber = "x"
+                FrequentFlyerNumber = "x123"
             };
 
             var decision = sut.Evaluate(application);
